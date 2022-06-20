ceb63c47e5aa707e6e03db5122df27f50f2b053f
iluckyday <luckyday1990@mail.com>
2022-06-20 09:06:23 +0800

    test

--- open5gs/UERANSIM.sh
+++ open5gs/UERANSIM.sh
@@ -4,7 +4,7 @@ set -ex
 LATEST_LTS=$(curl -skL https://releases.ubuntu.com | awk '($0 ~ "p-list__item") && ($0 !~ "Beta") {sub(/\(/,"",$(NF-1));print tolower($(NF-1));exit}')
 IMIRROR=${IMIRROR:-http://archive.ubuntu.com/ubuntu}
 
-include_apps+="git,make,cmake,gcc,g++,libsctp-dev,lksctp-tools"
+include_apps+="ca-certificates,git,make,cmake,gcc,g++,libsctp-dev,lksctp-tools"
 
 export DEBIAN_FRONTEND=noninteractiv
 apt update
