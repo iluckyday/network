5c1f744b83a65f18ca792eaef5f1af075724b407
iluckyday <luckyday1990@mail.com>
2023-02-28 10:29:28 +0800

    test

--- eve-com/mmdebstrap.sh
+++ eve-com/mmdebstrap.sh
@@ -7,14 +7,15 @@ IMIRROR=${IMIRROR:-http://archive.ubuntu.com/ubuntu}
 LINUX_KERNEL=linux-image-kvm
 
 include_apps="systemd,systemd-sysv,ca-certificates"
-include_apps+=",${LINUX_KERNEL},extlinux,initramfs-tools,busybox"
-include_apps+=",eve-ng"
-enable_services="systemd-networkd.service"
+include_apps+=",${LINUX_KERNEL},extlinux,initramfs-tools"
+include_apps+=",openssh-server"
+eve_apps="eve-ng"
+enable_services="systemd-networkd.service systemd-resolved.service ssh.service"
 disable_services="fstrim.timer motd-news.timer systemd-timesyncd.service"
 
 export DEBIAN_FRONTEND=noninteractive
 apt update
-apt install -y --no-install-recommends mmdebstrap qemu-utils
+apt install -y --no-install-recommends mmdebstrap qemu-system-x86 qemu-utils
 
 TARGET_DIR=/tmp/eve-com
 
@@ -26,7 +27,7 @@ mkfs.ext4 -F -L eve-com-root -b 1024 -I 128 -O "^has_journal" $loopx
 mkdir -p ${TARGET_DIR}
 mount $loopx ${TARGET_DIR}
 
-# wget -O - http://www.eve-ng.net/focal/eczema@ecze.com.gpg.key | sudo apt-key add -
+curl -skL https://www.eve-ng.net/focal/eczema@ecze.com.gpg.key | apt-key add -
 
 mmdebstrap --debug \
            --aptopt='Apt::Install-Recommends "false"' \
@@ -48,10 +49,10 @@ mmdebstrap --debug \
            --include=${include_apps} \
            ${EVE_UBUNTU_VERSION} \
            ${TARGET_DIR} \
-           "deb [trusted=yes] ${IMIRROR} ${EVE_UBUNTU_VERSION} main restricted universe multiverse" \
-           "deb [trusted=yes] ${IMIRROR} ${EVE_UBUNTU_VERSION}-updates main restricted universe multiverse" \
-           "deb [trusted=yes] ${IMIRROR} ${EVE_UBUNTU_VERSION}-security main restricted universe multiverse" \
-           "deb [trusted=yes] https://www.eve-ng.net/${EVE_UBUNTU_VERSION} ${EVE_UBUNTU_VERSION} main"
+           "deb ${IMIRROR} ${EVE_UBUNTU_VERSION} main restricted universe multiverse" \
+           "deb ${IMIRROR} ${EVE_UBUNTU_VERSION}-updates main restricted universe multiverse" \
+           "deb ${IMIRROR} ${EVE_UBUNTU_VERSION}-security main restricted universe multiverse" \
+           "deb https://www.eve-ng.net/${EVE_UBUNTU_VERSION} ${EVE_UBUNTU_VERSION} main"
 
 mount -t proc none ${TARGET_DIR}/proc
 mount -o bind /sys ${TARGET_DIR}/sys
@@ -104,19 +105,23 @@ DEFAULT eve-com
 LABEL eve-com
         LINUX /boot/vmlinuz
         INITRD /boot/initrd.img
-        APPEND root=LABEL=eve-com-root console=tty1 console=ttyS0 quiet intel_iommu=on iommu=pt
+        APPEND root=LABEL=eve-com-root console=tty1 console=ttyS0 quiet
 EOF
 
 chroot ${TARGET_DIR} /bin/bash -c "
 systemctl enable $enable_services
 systemctl disable $disable_services
+
 dd if=/usr/lib/EXTLINUX/mbr.bin of=$loopx
 extlinux -i /boot/syslinux
-dd if=/dev/zero of=/tmp/bigfile
-sync
-rm -f /tmp/bigfile
-sync
+ln -rsf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 "
+
+ssh-keygen -q -P '' -f /root/.ssh/id_ed25519 -C 'building' -t ed25519
+mkdir -p ${TARGET_DIR}/root/.ssh
+ssh-keygen -y -f /root/.ssh/id_ed25519 >> ${TARGET_DIR}/root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyuzRtZAyeU3VGDKsGk52rd7b/rJ/EnT8Ce2hwWOZWp" >> ${TARGET_DIR}/root/.ssh/authorized_keys
+chmod 600 ${TARGET_DIR}/root/.ssh/authorized_keys
 
 sync ${TARGET_DIR}
 umount ${TARGET_DIR}/dev ${TARGET_DIR}/proc ${TARGET_DIR}/sys
@@ -126,5 +131,51 @@ sleep 1
 umount ${TARGET_DIR}
 sleep 1
 losetup -d $loopx
+
+sleep 2
+systemd-run -G -q --unit qemu-eve-building.service qemu-system-x86_64 -name eve-building -machine q35,accel=kvm:hax:hvf:whpx:tcg -cpu kvm64 -smp "$(nproc)" -m 2G -display none -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 -boot c -drive file=/tmp/eve-com.raw,if=virtio,format=raw,media=disk -netdev user,id=n0,ipv6=off,hostfwd=tcp:127.0.0.1:22222-:22 -device virtio-net,netdev=n0
+
+sleep 180
+ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22222 -l root 127.0.0.1 bash -sx << SSHCMD
+sed -i '/building/d' /root/.ssh/authorized_keys
+apt update
+DEBIAN_FRONTEND=noninteractive apt install -y ${eve_apps}
+apt clean
+rm -rf /var/cache/apt/* /var/lib/apt/lists/*
+poweroff
+SSHCMD
+
+while [ true ]; do
+  pid=`pgrep eve-building || true`
+  if [ -z $pid ]; then
+    break
+  else
+    sleep 1
+  fi
+done
+
+sleep 1
+sync
+sleep 1
+
+loopx=$(losetup --show -f -P /tmp/eve-com.raw)
+mount $loopx ${TARGET_DIR}
+sleep 1
+chroot ${TARGET_DIR} /bin/bash -cx "
+dd if=/dev/zero of=/tmp/bigfile || true
+sync
+rm /tmp/bigfile
+sync
+"
+
+sleep 1
+killall provjobd || true
+sleep 1
+umount ${TARGET_DIR}
+sleep 1
+losetup -d $loopx
+sleep 1
+sync
+sleep 1
 
 qemu-img convert -c -f raw -O qcow2 /tmp/eve-com.raw /tmp/eve-com-${EVE_COM_VERSION}-${EVE_UBUNTU_VERSION}.img
